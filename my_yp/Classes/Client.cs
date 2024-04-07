5339ee5fb205b454338ccbb102328a46d9fadeff
Delon <winterrr.ad@gmail.com>
2024-04-07 21:17:39 +0500

    прописаны классы

---
+++ my_yp/Classes/Client.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualBasic.ApplicationServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace my_yp.Classes
+{
+    public class Client : User
+    {
+        public int ClientID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public Client(User user) : base(user.UserId, user.Username, user.Password, user.UserRole)
+        {
+            // Инициализация свойств клиента, если таковые имеются
+        }
+        public Client(int clientID, string name, string email)
+        {
+            ClientID = clientID;
+            Name = name;
+            Email = email;
+        }
+    }
+}
